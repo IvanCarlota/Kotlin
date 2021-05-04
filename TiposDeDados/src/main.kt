ad4513c2eb1c024347d9956acbf2d0e1626ab6ac
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-05-03 21:12:22 -0300

    Utilizando tipos de dados em kotlin

---
+++ TiposDeDados/src/main.kt
@@ -0,0 +1,31 @@
+/**
+ Type    Bit
+ Double  64
+ Float   32
+ Long    64
+ Int     32
+ Short   16
+ Byte    8
+ Boolean ?
+ String  ?
+ Char    ?
+ */
+
+/**Lembrando sobre unidades de medida
+
+1 Byte = 8 bits
+1 Kilobyte (kb) = 1024 bytes
+1 Megabyte (mb) = 1024 Kilobytes
+1 Gigabyte (gb) = 1024 Megabytes
+*/
+
+
+fun main() {
+    println("Double: Max:" + Double.MAX_VALUE + " - Double: Min:" + Double.MIN_VALUE)
+    println("Float: Max:" + Float.MAX_VALUE + " - Float: Min:" + Float.MIN_VALUE)
+    println("Long: Max:" + Long.MAX_VALUE + " - Long: Min:" + Long.MIN_VALUE)
+    println("Int: Max:" + Int.MAX_VALUE + " - Int: Min:" + Int.MIN_VALUE)
+    println("Short: Max:" + Short.MAX_VALUE + " - Short: Min:" + Short.MIN_VALUE)
+    println("Byte: Max:" + Byte.MAX_VALUE + " - Byte: Min:" + Byte.MIN_VALUE)
+
+}
