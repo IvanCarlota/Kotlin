77a59966f2e5afa919d1a217cac8dfbf1ad21086
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-05-06 21:16:56 -0300

    Variáveis mutáveis e variáveis imutáveis em kotlin

---
+++ VariaveisMutaveisEImutaveis/src/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // Variáveis mutáveis
+    var nome = "José da Silva"
+    nome = "João da Silva" +
+            ""
+    // Variáveis Imutáveis
+
+    val idade = 10
+
+    println(nome)
+}
