dcf1225388d75af20fe47e93e8a83268fecfed6a
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-05-07 21:56:41 -0300

    Formatando strings em kotlin.

---
+++ FormatandoStrings/src/main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val frase = "Kotlin é uma linguagem"
+
+    val caracteristica = "show!"
+
+    println("$frase ${caracteristica.length}")
+
+    val str = """jdioasjdiajsdoiajsdoajso  hdiuahsu jaiosjd
+        jdioasjdio jdaiosjdoa jdias dkfmasdkfm mdf mglfmg gdf jfjlçmr
+        ,flds slkmad.aç.;daçklsdas~dka~dma;sdm;adas
+        ,foasd´,falsdmpgmaspdmgapdm sdnaiksi ij
+    """.trimMargin()
+
+    println(str)
+}
