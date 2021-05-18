1c72938b67006cf4a557bed54034519dd2b72dd8
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-05-18 17:03:13 -0300

    Exercício mostrando o maior entre dois números

--- ExercicioFuncoes/src/main.kt
+++ ExercicioFuncoes/src/main.kt
@@ -1,4 +1,12 @@
+/*
+1. Crie uma função que recebe dois valores inteiros e imprima o maior deles. Considere
+que não serão informados dois valores iguais.
+ */
+
+fun maiorNumero (number1: Float, number2: Float) {
+    println("O maior número é: ${if (number1>number2) number1 else number2}")
+}
+
 fun main() {
-    val nome = "Ivan"
-    println("O nome ${nome} possui ${nome.length} caracteres")
+    maiorNumero(number1 = 10.0f, number2 = 15.5f)
 }
